146e8ebe5a19d0ea4acfc3798d37e012843d406f
KyleM426 <105526543+Kylem426@users.noreply.github.com>
2023-07-15 15:52:32 -0500

    Uploaded finished product.

--- library.js
+++ library.js
@@ -1,9 +1,8 @@
 let myLibrary = [];
 let form = document.getElementById('form-popup');
 let table = document.getElementById('table');
-let newBookBtn = document.getElementById('new');
-let cancelBtn = document.getElementById('cancel');
-let submitBtn = document.getElementById('submitBtn');
+let newBookBtn = document.getElementById('newBookBtn');
+let cancelBtn = document.getElementById('cancelBtn');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookPages = document.getElementById('pages');
@@ -15,13 +14,13 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.Info = function() {
-        return (title + ' ' + author + ' ' + pages + ' ' + read);
+        return (title + ' ' + author + ' It has ' + pages + ' pages ' + read);
     }
 };
 
 Book.prototype.changeReadStatus = function() {
     if(this.read == 'read') {
-        this.read = 'Not Read'
+        this.read = 'not read'
     }else {
         this.read = 'read'
     }
@@ -30,31 +29,56 @@ Book.prototype.changeReadStatus = function() {
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    console.log(myLibrary);
 };
 
 function displayBooks() {
     myLibrary.forEach(element => {
-        const bookNode = document.createElement('p');
-        const textNode = document.createTextNode(element.Info());
-        bookNode.dataset.id = element;
-        bookNode.appendChild(textNode);
-        table.appendChild(bookNode);
-    })
+        const bookHolder = document.createElement('div');
+        bookHolder.classList.add('bookholder');
+        const titleNode = document.createElement('p');
+        titleNode.innerHTML = element.title;
+        const authorNode = document.createElement('p');
+        authorNode.innerHTML = element.author;
+        const pagesNode = document.createElement('p');
+        pagesNode.innerHTML = element.pages;
+        const removeBtn = document.createElement('button');
+        const removeTxt = document.createTextNode('Remove');
+        removeBtn.classList.add('removeBtn');
+        removeBtn.addEventListener('click', () => {
+        let bookToRemove = findBookIndex(element);
+        removeBook(bookToRemove);
+        });
+        removeBtn.addEventListener('click', () => {
+            table.removeChild(bookHolder);
+        });
+        const readBtn = document.createElement('button');
+        readBtn.classList.add('readBtn');
+        const readTxt = document.createTextNode(element.read);
+        readBtn.addEventListener('click', () => {
+            element.changeReadStatus();
+            });
+        readBtn.addEventListener('click', () => {
+            if(readTxt.textContent == 'read') {
+                readTxt.textContent = 'not read'
+            }else {
+                readTxt.textContent = 'read'
+            }
+        });
+        removeBtn.appendChild(removeTxt); 
+        readBtn.appendChild(readTxt);  
+        bookHolder.appendChild(titleNode);
+        bookHolder.appendChild(authorNode);
+        bookHolder.appendChild(pagesNode);
+        bookHolder.appendChild(readBtn);
+        bookHolder.appendChild(removeBtn);
+        table.appendChild(bookHolder);
+    });
 };
-
-addBookToLibrary('test', 'this', 23, 'read');
-addBookToLibrary('second', 'test', 21, 'read');
-addBookToLibrary('third', 'test', 20, 'Not Read');
+addBookToLibrary('The Hobbit', 'by J.R.R. Tolkien', '304 pages', 'read');
+addBookToLibrary('Harry Potter and the Deathly Hallows', 'by J.K. Rowling', '784 pages', 'read');
+addBookToLibrary('Moby Dick', 'by Hermine Melville', '427 pages', 'not read');
 displayBooks();
-
-function displayLastBook() {
-    const lastItem = myLibrary[myLibrary.length -1];
-    const bookNode = document.createElement('p');
-    const textNode = document.createTextNode(lastItem.Info());
-    bookNode.dataset.id = myLibrary.length -1;
-    bookNode.appendChild(textNode);
-    table.appendChild(bookNode);
-};
 
 function openForm() {
     document.getElementById('form-popup').style.display = 'block';
@@ -75,7 +99,60 @@ form.onsubmit = function() {
     const pages = bookPages.value;
     const read = bookRead.value;
     addBookToLibrary(title, author, pages, read);
-    displayLastBook();
+    displayNewBook();
     closeForm();
     return false;
+};
+
+function findBookIndex(book_name) {
+    let index = myLibrary.findIndex(() => {book_name});
+    return index
+};
+
+function removeBook(index) {
+    myLibrary.splice(index, 1)
+};
+
+
+function displayNewBook() {
+    const newBook = myLibrary[myLibrary.length -1];
+    const bookHolder = document.createElement('div');
+    bookHolder.classList.add('bookholder');
+    const titleNode = document.createElement('p');
+    titleNode.innerHTML = newBook.title;
+    const authorNode = document.createElement('p');
+    authorNode.innerHTML = `by ${newBook.author}`;
+    const pagesNode = document.createElement('p');
+    pagesNode.innerHTML = `${newBook.pages} pages`;
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('removeBtn');
+    const removeTxt = document.createTextNode('Remove');
+    removeBtn.addEventListener('click', () => {
+    let bookToRemove = findBookIndex(newBook);
+    removeBook(bookToRemove);
+    });
+    removeBtn.addEventListener('click', () => {
+        table.removeChild(bookHolder);
+    });
+    const readBtn = document.createElement('button');
+    readBtn.classList.add('readBtn');
+    const readTxt = document.createTextNode(newBook.read);
+    readBtn.addEventListener('click', () => {
+        newBook.changeReadStatus();
+        });
+    readBtn.addEventListener('click', () => {
+        if(readTxt.textContent == 'read') {
+            readTxt.textContent = 'not read'
+        }else {
+            readTxt.textContent = 'read'
+        }
+    });
+    removeBtn.appendChild(removeTxt); 
+    readBtn.appendChild(readTxt);  
+    bookHolder.appendChild(titleNode);
+    bookHolder.appendChild(authorNode);
+    bookHolder.appendChild(pagesNode);
+    bookHolder.appendChild(readBtn);
+    bookHolder.appendChild(removeBtn);
+    table.appendChild(bookHolder);
 };
